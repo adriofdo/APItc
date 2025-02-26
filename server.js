0b0774e460cb508ab4c5a462b61da2cc75d0956d
adrio <tecnico@fenixtek.it>
2025-02-26 13:12:45 +0100

    ass

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST || 'servertc-25c772c3-adriotcplat2024.a.aivencloud.com',
+  user: process.env.DB_USER || 'avnadmin',
+  password: process.env.DB_PASSWORD || 'your_password_here',
+  database: process.env.DB_NAME || 'defaultdb',
+  port: process.env.DB_PORT || 20877,
   ssl: { rejectUnauthorized: true } // Required for Aiven
 });
 
